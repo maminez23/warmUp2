0c07073787cfc127dfcd40a7b9508bc2c21742ce
maminez23 <amineziadi23@gmail.com>
2020-02-28 09:23:53 +0100

    Amine

--- warmUp20/warmUp-20.js
+++ warmUp20/warmUp-20.js
@@ -4,3 +4,12 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(str){
+    var arr = str.split(' ');
+    var result = arr[arr.length - 2];
+    for(var i = arr.length - 1; i >= 0; i--){
+        result = result + " " +arr[i];
+    }
+    return result;
+}
